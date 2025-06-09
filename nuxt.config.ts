2579d4a93e5035a063bf6116dfa197862ca7ea58
Thin Htet Htet Zaw <berry@Thins-Air.lan>
2025-06-09 17:00:48 +0700

    nuxt tailwind setup

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  components: {
+    dirs: ["components"],
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
